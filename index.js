44cd0184688cb4238fa20ebe94b57ffe37f7465e
amgarciav <formacionsena2223@gmail.com>
2025-05-14 20:03:39 -0500

    se modifico el archivo index.js se creo la conexion a la base de datos

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express =require('express');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-app.get('/', (req, res) =>{
-    res.send('servidor funcionado');
-   
+const postRoute=require('./routes/posts');
+app.use('/servicios', postRoute);
 
+mongoose.connect('mongodb+srv://sena2225:N7OxCaUtjvW7KVH2@formacion.nxr7elr.mongodb.net/post?retryWrites=true&w=majority&appName=formacion', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
-
 
 app.listen(10000);
